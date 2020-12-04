340a5eb71ff82d3eff452598a43c9c20800b656b
Abhisheks15 <abhishek6307100065@gmail.com>
2020-12-04 16:07:54 +0530

    create student model

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace WebAppAzureExer01.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
